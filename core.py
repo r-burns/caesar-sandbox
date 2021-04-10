2a413e987fd091996766e54502071d44c0bef6b1
Ryan Burns <rtburns@protonmail.com>
2021-04-09 20:50:38 -0700

    Remove unused python Orbit

--- core.py
+++ core.py
@@ -1,5 +1,3 @@
-from scipy.interpolate import CubicSpline
-
 class Interval:
     def __init__(self, min, max):
         self.min = min
@@ -15,16 +13,3 @@ class LinearSpace:
 
     def index_of(self, value):
         return (value - self.start) / self.spacing
-
-class Orbit:
-    def __init__(self, start, spacing, posvel):
-        times = [start + (spacing * i) for i in range(len(posvel))]
-        self.data = CubicSpline(times, posvel)
-
-    # get the azimuth times associated with each sample
-    def times(self):
-        return self.data.x
-
-    # get the state-vector at a given azimuth time
-    def __call__(self, t):
-        return self.data(t)
